30f9d7a3779d8b53aa5ca2049a0b0e1a63e06252
Shuying Liu <shuyingliu24@gmail.com>
2022-10-06 21:13:15 +0100

    modified A and S, added Q10

--- proj1.R
+++ proj1.R
@@ -14,19 +14,19 @@
 #XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 # Please change the below to your local repo directory
-setwd("D:/Edinburgh/Courses_study/Statistical programming/Project1/proj1")
+setwd("D:\\Edinburgh\\Statistical Programming\\proj1")
 
 a <- scan("pg10.txt",what="character",skip=104) ## skip contents
 n <- length(a)
 a <- a[-((n-2886):n)] ## strip license
 a <- a[-grep("[0123456789]:[0123456789]",a)] ## strip out verse numbers
 
-############################ Q 4-5 ##########################################
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXX Question 4-5 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 # Pre-process a, remove special punctuations
 a <- gsub("[()*]","",a)
 
-#separate required punctuations
+# Separate required punctuations
 split_punct <- function(old,punct){
   rep_i <- rep(1,length(old))
   punct_i <- grep(punct,old,fixed=TRUE)
@@ -39,10 +39,9 @@ split_punct <- function(old,punct){
 
 for (punct in c(",",".",";","!",":","?")) a <- split_punct(a,punct)
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Question 6 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX Question 6, 10 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-# a <- split_punct(a)
-# a>a, lowercase>lower_a
+# Generate b containing most common words
 len_splited <- length(a)
 lower_a <- tolower(a)
 
@@ -52,7 +51,7 @@ uniq_match <- match(lower_a,uniq_w) # Match bible to the unique words
 
 freq_uniq <- tabulate(uniq_match) # Count the frequeny of times of occuring words
 
-# Choosing the top 500 most common words
+# Choosing the top 500 most common words and storing them in b
 m <- 500
 
 up_order <- order(freq_uniq) # Sort the freq_uniq in ascending order
@@ -61,115 +60,83 @@ most_500 <- up_order[(length(up_order)- m + 1):length(up_order)] # The index of 
 
 b <- uniq_w[most_500]
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# Identify words that most often start with a capital letter, then modify b
+b_new <- b
+for (i in 1:m){
+  a_word <- a[lower_a==b_new[i]]
+  if (mean(a_word!=b[i])>0.5) substr(b_new[i],1,1) <- toupper(substr(b_new[i],1,1))
+}
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Question 7  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Generating T  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-match_T <- match(lowercase,b)
-shift_T <- cbind(match_T[1:(length(match_T)-2)],match_T[2:(length(match_T)-1)],match_T[3:length(match_T)])
-pairT_index <- shift_T[!is.na(rowSums(shift_T)),] # Find the location of NA
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Question 7  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXXXXXXXXXXXXXXXX  Generating T, A, S  XXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-T <- array(0, dim = c(m,m,m)) # Define an m by m by m matrix
+match_T <- match(lower_a,b)
+# c1, c2, c3 store the adjacent common words
+c3 <- cbind(match_T[1:(length(match_T)-2)],match_T[2:(length(match_T)-1)],match_T[3:length(match_T)])
+c2 <- c3[,c(1,2)]
+c1 <- c3[,1]
 
+# Remove the rows with NA
+cw3 <- c3[!is.na(rowSums(c3)),]
+cw2 <- c2[!is.na(rowSums(c2)),]
+cw1 <- c1[!is.na(c1)]
+
+# Initialize T, A, S to matrices of zeros
+T <- array(0,c(m,m,m))
+A <- array(0,c(m,m))
+S <- array(0,m)
+
+# Generate T, A, S
 # When i,k,j is a common_pair(j joins after i and k), the time increases by one
-for (i in 1:length(pairT_index)){
-  T[shift_T[pairT_index[i],1], shift_T[pairT_index[i],2], shift_T[pairT_index[i],3]] <- T[shift_T[pairT_index[i],1], shift_T[pairT_index[i],2], shift_T[pairT_index[i],3]] + 1
+for (rnum in 1:nrow(cw3)){
+  T[cw3[rnum,1],cw3[rnum,2],cw3[rnum,3]] <- T[cw3[rnum,1],cw3[rnum,2],cw3[rnum,3]]+1
+}
+for (rnum in 1:nrow(cw2)){
+  A[cw2[rnum,1],cw2[rnum,2]] <- A[cw2[rnum,1],cw2[rnum,2]]+1
+}
+for (i in 1:length(cw1)){
+  S[cw1[i]] <- S[cw1[i]]+1
 }
 
-# Let the sum of each row's probability equal to one
-for (i in 1:m){
-  for (k in 1:m){
-    if (sum(T[i,k,])!=0){
-      T[i,k,] <- T[i,k,]/sum(T[i,k,])
-    }
-  }
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Question 8  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+# Generate a 50-word text from T, A, S
+# Randomly pick two words to start generating text
+word_i <- sample(1:m,size=1,prob=S)
+if (sum(A[word_i,])){
+  word_k <- sample(1:m,size=1,prob=A[word_i,])
+}else{
+  word_k <- sample(1:m,size=1,prob=S)
 }
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Generating A  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-match_A <- match(lowercase,b)
-
-shift_A <- cbind(match_A[1:length(match_A)-1],match_A[2:length(match_A)])
-
-pairA_index <- grep(FALSE,is.na(rowSums(shift_A))) # Find the location of NA
-
-A <- matrix(0, nrow= m, ncol=m) # Define an m by m matrix
-
-# if i,j is a common_pair, the time plus one
-for (i in 1:length(pairA_index)){
-  A[shift_A[pairA_index[i],1], shift_A[pairA_index[i],2]] <- A[shift_A[pairA_index[i],1], shift_A[pairA_index[i],2]] + 1
-}
-
-# Let the sum of each row's probability equal to one
-for (i in 1:m){
-  A[i,] <- A[i,]/sum(A[i,])
-}
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Generating S  XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-match_S <- match(lowercase,b)
-
-shift_S <- cbind(match_S)
-
-pairS_index<- grep(FALSE,is.na(rowSums(shift_S)))
-
-S<-array(0, dim = c(m)) # Define an m rows' matrix
-
-for (i in 1:length(pairS_index)){
-  S[shift_S[pairS_index[i]]] <- S[shift_S[pairS_index[i]]] + 1
-}
-
-# Let the sum of each row's probability equal to one
-for (i in 1:m){
-  S[i] <- S[i]/sum(S)
-}
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Question 8  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-rand_b <- 1:length(b)
-rand_index <- sample(rand_b,size = 2,replace = TRUE) # Two random words from b are used as the premises of the quadratic Markov chain
 
 # Complete the composition of sentences in the Bible by Markov chain
-sample_index <- rep(0,50)
-sample_index[1] <- rand_index[1]
-sample_index[2] <- rand_index[2]
+sample_index <- array(0,50)
+sample_index[1:2] <- c(word_i,word_k)
 
 for (i in 3:50)
 {
-  if (sum(T[sample_index[i-2],sample_index[i-1],])!=0){
-    rand_index <- sample(1:length(b),size = 1,prob=T[sample_index[i-2],sample_index[i-1],])
+  if (sum(T[sample_index[i-2],sample_index[i-1],])){
+    rand_index <- sample(1:m,size = 1,prob=T[sample_index[i-2],sample_index[i-1],])
   }
-  else if(sum(A[sample_index[i-1],]) != 0){
-    rand_index <- sample(1:length(b),size = 1,prob=A[sample_index[i-1],])
+  else if(sum(A[sample_index[i-1],])){
+    rand_index <- sample(1:m,size = 1,prob=A[sample_index[i-1],])
   }
   else{
-    rand_index <- sample(1:length(b),size = 1,prob=S[sample_index[i]])
-    
+    rand_index <- sample(1:m,size = 1,prob=S)
   }
   sample_index[i] <- rand_index
 }
 
-cat(b[sample_index]) # Print out biblical sentences formed by quadratic Markov chains
+# Print out biblical sentences formed by quadratic Markov chains
+cat("Text generated from T, A and S:\n\n")
+cat(b[sample_index],"\n\n(q10)\n")
+cat(b_new[sample_index],"\n\n")
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+#XXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Question 9  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXX  Question 9  XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+# Generate a 50-word text from just S
+rand_index_S <- sample(1:m,size=50,replace=TRUE,prob=S)
 
-b_form_S <- 1:length(b)
-rand_index_S <- sample(b_form_S,size = 1, prob = S)
+cat("Text generated from just S:\n\n")
+cat(b[rand_index_S])
 
-# Create Biblical sentences only through S
-# Since Markov chain is not used, it does not need to pick the first word,
-# Just randomly pick the first word by probability
-sample_S_index <- rep(0,50)
-sample_S_index[1] <-  rand_index_S
-
-for (i in 2:50)
-{
-  rand_index_S <- sample(1:length(b), size = 1, prob = S)
-  sample_S_index[i] <- rand_index_S
-}
-
-cat(b[sample_S_index]) # Print out biblical sentences just by S
-
-#XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
